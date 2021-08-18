144df8b07e6015fad6d240ce7d6a8ab8eb3ec287
Mavine Naaman <you@example.com>
2021-08-18 11:10:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./features/User/Login"
+import Signup from "./features/User/Signup"
+import Dashboard from "./features/User/Dashboard"
+import { PrivateRoute } from "./Routes/PrivateRoute"
+function App() {
+  return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact component={Login} path="/login" />
+            <Route exact component={Signup} path="/signup" />
+            <PrivateRoute exact component={Dashboard} path="/" />
+          </Switch>
+        </Router>
+      </div>
+  )
+}
+export default App
